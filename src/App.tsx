e6a3790f06d251fedaf6a3fb831421188b0a2318
ferrixventures-account <ferrix.ventures@gmail.com>
2025-07-12 14:48:45 -0500

    Update No. 29

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import { ThemeProvider } from '@/theme/ThemeContext';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 
 const queryClient = new QueryClient();
@@ -30,6 +31,7 @@ const App = () => (
               <Route path="*" element={<NotFound />} />
             </Routes>
           </BrowserRouter>
+          <SpeedInsights />
         </div>
       </TooltipProvider>
     </QueryClientProvider>
