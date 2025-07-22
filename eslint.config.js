63ffa9a0d6b6067de134812b90c9a2b67103388b
ferrixventures-account <ferrix.ventures@gmail.com>
2025-07-22 08:52:55 -0500

    Update No. 42

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,6 @@
+import nextPlugin from "@next/eslint-plugin-next";
 import js from "@eslint/js";
 import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
@@ -14,15 +13,11 @@ export default tseslint.config(
       globals: globals.browser,
     },
     plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
+      "@next/next": nextPlugin,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs["core-web-vitals"].rules,
       "@typescript-eslint/no-unused-vars": "off",
     },
   }
